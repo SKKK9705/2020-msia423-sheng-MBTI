664a71a40a3741993857b142c5bc45ea86af08e7
Kailang Sheng <kailangsheng2020@u.northwestern.edu>
2020-05-15 14:51:53 -0600

    midterm checkpoint

--- config/flaskconfig.py
+++ config/flaskconfig.py
@@ -1,9 +1,21 @@
+import os
+
 DEBUG = True
 LOGGING_CONFIG = "config/logging/local.conf"
 PORT = 5000
-APP_NAME = "penny-lane"
-SQLALCHEMY_DATABASE_URI = 'sqlite:///data/tracks.db'
+APP_NAME = "mbti-prediction"
+SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 HOST = "0.0.0.0"
 SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
 MAX_ROWS_SHOW = 100
+
+conn_type = "mysql+pymysql"
+user = os.environ.get("MYSQL_USER")
+password = os.environ.get("MYSQL_PASSWORD")
+host = os.environ.get("MYSQL_HOST")
+port = os.environ.get("MYSQL_PORT")
+DATABASE_NAME = 'msia423_db'
+
+if SQLALCHEMY_DATABASE_URI is not None:
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.format(conn_type=conn_type, user=user, password=password, host=host, port=port, DATABASE_NAME=DATABASE_NAME)
